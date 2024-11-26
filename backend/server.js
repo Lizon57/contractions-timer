8fd0dde32176fbc897e521542e64d1b684d75d13
Oren Yaniv <51949130+Lizon57@users.noreply.github.com>
2024-11-26 10:20:17 +0200

    Require path package before using it - bf

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
+const path = require('path')
 const app = express()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 
